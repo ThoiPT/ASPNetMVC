f8bb5c6ae5d638682e57f3ae3c3d426fc90e5ed4
ThoiPT <lephatloc2016@gmail.com>
2022-11-21 15:01:53 +0700

    first commit

---
+++ ArtDecorASP/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtDecorASP.Areas.Admin.Controllers
+{
+    // Định danh cho Controller
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
